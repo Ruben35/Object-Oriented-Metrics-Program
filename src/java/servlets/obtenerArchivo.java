8e2e98ee8febb3a77ca42ce18edde66aad09e0bd
Ruben35 <ruben111416@gmail.com>
2020-02-12 11:51:26 -0600

    Adding web.xml

--- src/java/servlets/obtenerArchivo.java
+++ src/java/servlets/obtenerArchivo.java
@@ -1,15 +1,13 @@
-package servlets;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,7 +16,6 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Ruben35
  */
-@WebServlet(urlPatterns = {"/obtenerArchivo"})
 public class obtenerArchivo extends HttpServlet {
 
     /**
