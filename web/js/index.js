53dcee144499accb28b36387935c3e75906ef007
Ruben35 <ruben111416@gmail.com>
2020-02-22 15:06:11 -0600

    Adding validations and improving style

--- web/js/index.js
+++ web/js/index.js
@@ -5,10 +5,50 @@
  */
 
 function showFileName( event ) {
-     var input = event.srcElement;
-     var file=document.getElementById("nom-file");
-     var e=input.files.length;
-     
-     file.textContent = input.files[0].name;
+    var input = event.srcElement;
+    var no_files=document.getElementById("no-files");
+    var name_files=document.getElementById("name-files");
+    var name_files_list=document.getElementById("name-files-list");
+    var boton=document.getElementById("boton");
+    no_files.style.display="none";
+    name_files.style.display="";
+    name_files_list.innerHTML="";
+    boton.disabled=false;
+    for(var i=0;i<input.files.length;i++){
+        name_files_list.innerHTML+="<p class='list-item is-gray'>"+input.files[i].name+"</p>";
+    }
+}
+
+function disableAll(){
+    var no_files=document.getElementById("no-files");
+    var name_files=document.getElementById("name-files");
+    var boton=document.getElementById("boton");
+    no_files.style.display="";
+    name_files.style.display="none";
+    boton.disabled=true;
+}
+
+function fileValidation(event){
+    var fileInput = document.getElementById('file-input');
+    var filePath = fileInput.value;
+    var allowedExtensions = /(.java)$/i;
+    if(!allowedExtensions.exec(filePath)){
+        alert('Agrega solo archivos con extensión .java');
+        fileInput.value = '';
+        disableAll();
+        return false;
+    }else{
+        showFileName(event);
+    }
+}
+
+function refresh() {
+    var e = document.getElementById("refreshed");
+    if (e.value === "no")
+        e.value = "yes";
+    else {
+        e.value = "no";
+        location.reload();
+    }
 }
 
